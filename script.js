887baab631216308c20be630d2a310e97e645083
MoisesKersch <34980935+MoisesKersch@users.noreply.github.com>
2018-09-17 16:17:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+/* Adding the script tag to the head as suggested before */
+
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
+
+// Then bind the event to the callback function.
+// There are several events for cross browser compatibility.
+script.onreadystatechange = handler;
+script.onload = handler;
+
+// Fire the loading
+head.appendChild(script);
+
+function handler() {
+    // attach handler to form's submit event 
+    $('#myFormId').submit(function () 
+    {
+        var queryString = $('#myFormId').serialize(); 
+        console.log(queryString);
+    });
+}
